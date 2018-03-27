664a245093f86bc84c4e71d7c9d9897e32531f81
Gabriel Majivu <gmajivu@gmail.com>
2018-03-27 12:55:37 +0300

    a more concise arrow function

--- src/App.js
+++ src/App.js
@@ -22,8 +22,7 @@ class App extends Component {
       }
     ]
 
-    const listItems = list.map((item) => {
-      return (
+    const listItems = list.map(item => {
         <div key={item.objectID}>
           <span>
             <a href={item.url}>{item.title}</a>
@@ -32,7 +31,6 @@ class App extends Component {
           <span>{item.num_comments}</span>
           <span>{item.points}</span>
         </div>
-      );
     });
 
     return (
