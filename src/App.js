985f72946db8507e96574d1035242d0a12ea3443
Gabriel Majivu <gmajivu@gmail.com>
2018-03-27 14:34:39 +0300

    return is needed here

--- src/App.js
+++ src/App.js
@@ -23,6 +23,7 @@ class App extends Component {
     ]
 
     const listItems = list.map(item => {
+      return (
         <div key={item.objectID}>
           <span>
             <a href={item.url}>{item.title}</a>
@@ -31,6 +32,7 @@ class App extends Component {
           <span>{item.num_comments}</span>
           <span>{item.points}</span>
         </div>
+      );
     });
 
     return (
