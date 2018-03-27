7efefeb7fe7ef8f3fa19e50a12d48fbf78e14f7f
Gabriel Majivu <gmajivu@gmail.com>
2018-03-27 12:27:24 +0300

    refactor for expressions with nested curly braces

--- src/App.js
+++ src/App.js
@@ -22,20 +22,22 @@ class App extends Component {
       }
     ]
 
+    const listItems = list.map((item) => {
+      return (
+        <div key={item.objectID}>
+          <span>
+            <a href={item.url}>{item.title}</a>
+          </span>
+          <span>{item.author}</span>
+          <span>{item.num_comments}</span>
+          <span>{item.points}</span>
+        </div>
+      );
+    });
+
     return (
       <div className="App">
-        {list.map(function(item) {
-          return (
-            <div key={item.objectID}>
-              <span>
-                <a href={item.url}>{item.title}</a>
-              </span>
-              <span>{item.author}</span>
-              <span>{item.num_comments}</span>
-              <span>{item.points}</span>
-            </div>
-          );
-        })}
+        {listItems}
       </div>
     );
   }
