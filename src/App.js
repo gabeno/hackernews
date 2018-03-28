6bddfda6cdf52722cd60fe87c708deab6e7be0c8
Gabriel Majivu <gmajivu@gmail.com>
2018-03-28 12:19:18 +0300

    make input field a controlled component - state single source of truth

--- src/App.js
+++ src/App.js
@@ -74,7 +74,7 @@ class App extends Component {
     return (
       <div className="App">
         <form>
-          <input type="text" onChange={this.onSearchChange} />
+          <input type="text" onChange={this.onSearchChange} value={searchTerm} />
         </form>
         {list.filter(isSearched(searchTerm)).map(item =>
           <div key={item.objectID}>
