1da83355b77b53ac0521c745ef09cc7d43da0200
Gabriel Majivu <gmajivu@gmail.com>
2018-04-01 18:52:56 +0300

    add prop types to components to make them type safe

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import axios from 'axios';
+import PropTypes from 'prop-types';
 import './App.css';
 
 const DEFAULT_QUERY = 'Redux';
@@ -166,6 +167,13 @@ const Search = ({ value, onChange, children, onSubmit }) =>
     <button type="submit">{children}</button>
   </form>
 
+Search.propTypes = {
+  value: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+  children: PropTypes.node,
+  onSubmit: PropTypes.func.isRequired,
+};
+
 const Table = ({ list, onDismiss }) =>
   <div className="table">
     {list.map(item =>
@@ -185,11 +193,29 @@ const Table = ({ list, onDismiss }) =>
     )}
   </div>
 
+Table.propTypes = {
+  list: PropTypes.arrayOf(
+    PropTypes.shape({
+      objectID: PropTypes.string.isRequired,
+      author: PropTypes.string,
+      url: PropTypes.string,
+      num_comments: PropTypes.number,
+      points: PropTypes.number,
+    })
+  ).isRequired,
+  onDismiss: PropTypes.func.isRequired,
+};
+
 const Button = ({ onClick, className = '', children }) =>
   <button onClick={onClick} className={className} type="button">
     {children}
   </button>
 
+Button.propTypes = {
+  onClick: PropTypes.func.isRequired,
+  className: PropTypes.string,
+  children: PropTypes.node.isRequired,
+};
 
 /*
 class Component {
