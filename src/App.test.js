10a7bf5908979647f09076fe929b9e4dd3e6bb64
Gabriel Majivu <gmajivu@gmail.com>
2018-04-02 13:49:39 +0300

    fix tests and add one for Loading component

--- src/App.test.js
+++ src/App.test.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import renderer from 'react-test-renderer';
 import Enzyme, { shallow } from 'enzyme';
 import Adapter from 'enzyme-adapter-react-16';
-import App, { Search, Button, Table } from './App';
+import App, { Search, Button, Table, Loading } from './App';
 
 Enzyme.configure({ adapter: new Adapter() });
 
@@ -22,37 +22,48 @@ describe('App', () => {
 });
 
 describe('Search', () => {
+  const props = {
+    value: 'something',
+    onChange() {},
+    onSubmit() {}
+  };
+
+  const Element = <Search {...props}>Search</Search>;
+
   it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(<Search>Search</Search>, div);
+    ReactDOM.render(Element, div);
     ReactDOM.unmountComponentAtNode(div);
   });
 
   test('has a valid snapshot', () => {
-    const component = renderer.create(
-      <Search>Search</Search>
-    );
+    const component = renderer.create(Element);
     let tree = component.toJSON();
     expect(tree).toMatchSnapshot();
   });
 });
 
 describe('Button', () => {
-  const button = <Button>Give me more</Button>;
+  const props = {
+    onClick() {}
+  };
+
+  const Element = <Button {...props}>Give me more</Button>;
+
   it('renders without crashing', () => {
     const div = document.createElement('div');
-    ReactDOM.render(button, div);
+    ReactDOM.render(Element, div);
     ReactDOM.unmountComponentAtNode(div);
   });
 
   test('has a valid snapshot', () => {
-    const component = renderer.create(button);
+    const component = renderer.create(Element);
     let tree = component.toJSON();
     expect(tree).toMatchSnapshot();
   });
 
   it('shows passed text', () => {
-    const element = shallow(button);
+    const element = shallow(Element);
     expect(element.text()).toEqual('Give me more');
   });
 });
@@ -60,9 +71,10 @@ describe('Button', () => {
 describe('Table', () => {
   const props = {
     list : [
-      {title: 1, author: 1, num_comments: 1, points: 2, objectID: 'y'},
-      {title: 2, author: 2, num_comments: 1, points: 2, objectID: 'z'}
-    ]
+      {title: 1, author: 'a', num_comments: 1, points: 2, objectID: 'y'},
+      {title: 2, author: 'b', num_comments: 1, points: 2, objectID: 'z'}
+    ],
+    onDismiss() {}
   };
   it('renders without crashing', () => {
     const div = document.createElement('div');
@@ -82,4 +94,21 @@ describe('Table', () => {
     const element = shallow(<Table {...props} />);
     expect(element.find('.table-row').length).toBe(2);
   });
+});
+
+describe('Loading', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Loading />, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+
+  test('has a valid snapshot', () => {
+    const component = renderer.create(
+      <Loading />
+    );
+    let tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
+
 });
